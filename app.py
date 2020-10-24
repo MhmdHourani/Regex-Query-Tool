c56d0c5799d490f6ef22c7fc17755e34e94ff137
Mohammed Hourani <m.hourani98@gmail.com>
2020-10-24 21:02:45 +0300

    add some

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+from regexQuery import regex_query
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "this is the home page"
+    return render_template('index.html')
+
+
+@app.route('/result', methods=["POST", "GET"])
+def result():
+    if request.method == "POST":
+        query = request.form['regex']
+        text = request.form['test_str']
+        result = regex_query(query, text)
+        if result == ([], []):
+            print(result3)
+        # if not any(result):
+        #     # print("This is the result inside the len() and it is empty")
+        #     return "Sorry there no result, try again"
+        # else:
+        return jsonify(regex_query(query, text))
+    else:
+        return "This is the submit page mothe GET"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
---
+++ regexQuery.py
@@ -0,0 +1,24 @@
+import re
+
+
+def regex_query(regex, test_string):
+    """"""
+    list_tuples_indices = []
+    matches = re.finditer(regex, test_string, re.MULTILINE)
+
+    list_matches = []
+    for matchNum, match in enumerate(matches, start=1):
+        print("Match {matchNum} was found at {start}-{end}: {match}".format(
+            matchNum=matchNum, start=match.start(), end=match.end(), match=match.group()))
+        tuple_start_end_indices = match.start(), match.end()
+        list_tuples_indices.append(tuple_start_end_indices)
+
+        list_matches.append(match.group())
+    return list_matches, list_tuples_indices
+    # return list_matches
+
+
+# regex = r"([A-Z])\w+"
+
+# test_str = "Lorem ipsum dolor sit amet, nam no malis graecis incorrupte. Dicunt insolens sed ex, ut saepe quaerendum eam. Eum no quas tempor impetus, in per expetendis reprimique mediocritatem. Meis erant facilisi cu ius, utroque moderatius voluptatibus no nam. Pri an graecis torquatos accommodare. Quodsi everti ei qui"
+# print(regex_query(regex, test_str))
